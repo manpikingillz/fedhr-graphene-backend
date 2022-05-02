68e8659bb97eb246bd632f1f181b390a639c7570
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-05-02 04:11:13 +0300

    add resolver for first name

--- fedhr/schema.py
+++ fedhr/schema.py
@@ -6,12 +6,16 @@ from employment.models import Employee
 class EmployeeType(DjangoObjectType):
     class Meta:
         model = Employee
-        fields = '__all__'
+        fields = ('first_name', 'last_name')
 
 class Query(graphene.ObjectType):
     all_employees = graphene.List(EmployeeType)
+    employee_by_first_name = graphene.Field(EmployeeType, first_name=graphene.String())
 
     def resolve_all_employees(root, info):
         return Employee.objects.all()
+    
+    def resolve_employee_by_first_name(root, info, first_name):
+        return Employee.objects.get(first_name=first_name)
 
 schema = graphene.Schema(query=Query)
