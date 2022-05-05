547b45fd4145e40e3d57e2195b647ca24288e737
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-05-05 05:54:11 +0300

    add tests for employee model

--- employment/models.py
+++ employment/models.py
@@ -13,3 +13,11 @@ class Employee(models.Model):
     middle_name = models.CharField(max_length=255, null=True, blank=True)
     date_of_birth = models.DateTimeField(null=True, blank=True)
     gender =  models.IntegerField(choices=GENDER_CHOICES, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+    def full_name(self):
+        middle_name = self.middle_name if self.middle_name else ''
+        full_name = f'{self.first_name} {middle_name} {self.last_name}'
+        return ' '.join(full_name.split())
--- employment/tests.py
+++ employment/tests.py
@@ -1,63 +1,14 @@
 
-import json
-import pytest
-from graphene_django.utils.testing import graphql_query
-from fedhr.schema import schema
 from employment.models import Employee
-from graphene.test import Client
 
-# https://github.com/graphql-python/graphene/blob/master/examples/starwars/tests/test_query.py
-# https://www.velotio.com/engineering-blog/use-pytest-fixtures-with-django-models
-# https://fithis2001.medium.com/testing-graphql-for-the-beginner-pythonistas-79cdda9b722c
-# https://www.programcreek.com/python/example/104864/graphene.Schema
-# https://pytest-django.readthedocs.io/en/latest/helpers.html#assertions
-# https://github.com/graphql-python/graphene-django/blob/main/examples/starwars/tests/test_connections.py
+def test_string_representation():
+    employee = Employee(first_name='Ambrose', last_name='Tumwijukye')
+    assert str(employee) == 'Ambrose Tumwijukye'
 
-# Make video when the test is successful
+def test_employee_full_name_with_all_three_names():
+    employee = Employee(first_name='Ambrose', middle_name='Savimbi', last_name='Tumwijukye')
+    assert employee.full_name() == 'Ambrose Savimbi Tumwijukye'
 
-# https://stackoverflow.com/questions/9574810/writing-test-cases-for-django-models
-# 1. Testing that the model has the fields it should have
-
-# 2. Test Queries
-
-# 3. Test Mutations
-
-# @pytest.fixture
-# def employee(db) -> Employee:
-#     employee = Employee.objects.create(first_name="Barnabas", last_name="Tumwekwase")
-#     # print(f'employee: {employee}')
-#     assert employee.first_name == "Barnabas"
-    
-# def test_filter_employee(employee):
-#     assert Employee.objects.filter(first_name='Barnabas').exists()
-    
-# def test_update_employee(employee):
-#     print(f'employee: {employee}')
-    # employee.first_name = "Gilbert"
-    # employee.save()
-    # updated_employee = Employee.objects.get(first_name="Gilbert")
-    # assert updated_employee.first_name == "Gilbert"
-    
-
-client = Client(schema)
-
-@pytest.mark.django_db
-def test_all_employees_query():
-    Employee.objects.create(first_name='Ambrose', last_name='Tumwijukye')
-    query = '''
-        query {
-            allEmployees {
-                firstName
-                lastName
-            }
-        }
-
-        '''
-    result = client.execute(query)
-    # result = schema.execute(query)
-    print(f'result: {result}')
-    assert 'errors' not in result
-    
-# def test_create_employee()
-
-
+def test_employee_full_name_with_only_two_names():
+    employee = Employee(first_name='Ambrose', last_name='Tumwijukye')
+    assert employee.full_name() == 'Ambrose Tumwijukye'
