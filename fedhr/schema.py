eb9d9a8aba457bb241502fe2bc463cff7b542c64
Gilbert Twesigomwe <gilbert@bposeats.com>
2022-05-06 01:47:33 +0300

    mutation to create employee

--- fedhr/schema.py
+++ fedhr/schema.py
@@ -18,4 +18,32 @@ class Query(graphene.ObjectType):
     def resolve_employee_by_first_name(root, info, first_name):
         return Employee.objects.get(first_name=first_name)
 
-schema = graphene.Schema(query=Query)
+# class GenderEnum(graphene.Enum):
+#     MALE = 1
+#     MALE = 2
+
+class CreateEmployee(graphene.Mutation):
+    class Arguments:
+        first_name = graphene.String(required=True)
+        middle_name = graphene.String(required=False)
+        last_name = graphene.String(required=True)
+        # date_of_birth = graphene.DateTime(required=False)
+        # gender = graphene.Enum.from_enum(GenderEnum)
+
+    employee = graphene.Field(EmployeeType)
+
+    @classmethod
+    def mutate(cls, root, info, first_name, middle_name, last_name):
+        employee = Employee(
+            first_name=first_name,
+            middle_name=middle_name,
+            last_name=last_name
+        )
+        employee.save()
+        return CreateEmployee(employee=employee)
+
+class Mutation(graphene.ObjectType):
+    create_employee = CreateEmployee.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
